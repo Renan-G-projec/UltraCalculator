942b6f268fedfacc4b3c77f4018fae107cd93b5e
Renan G <renanfartes@gmail.com>
2025-08-25 20:36:27 -0300

    Updating resources

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-const buttons = window.document.getElementsByClassName("change-type")
+const buttons = document.getElementsByClassName("change-type")
+let main_area = document.getElementById("calc-are")
+
+// Makes the default calculator be showed on the screen when the user press the default button
+function set_default() {
+
+}
+
+// For the Atom calculator:
+function set_atom() {
+
+}
+
+// For the velocity one:
+function set_velocity() {
+    main_area.style.borderRadius = "20px"
+    main_area.style.backgroundColor = "white"
+    main_area.style.height = "20px"
+    main_area.style.width = "20px"
+}
