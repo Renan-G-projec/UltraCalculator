44bcbb9c7e80095f94f6b16cb4f5ae87541055d0
Renan G <renanfartes@gmail.com>
2025-08-28 12:20:56 -0300

    Updated Style and Js of UltraCalculator
    The calculator is now in the correct position on the page and it now pu the numbers and operations on the screen. Updated also the README.md file.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# UltraCalculator
-That's a math project with 7 types of calculators.
+# Ultracalculator
+This is a math web project for the Summer of Making 2025, composed of 7 types of calculator:
+* Basic calculator;
+* Atoms calculator
+* Velocity calculator
+* Exchange calculator
+* Proporcionality calculator
+* Function plot calculator
+* Mol calculator
+Each type will be specified soon.
--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-const buttons = document.getElementsByClassName("change-type")
-let main_area = document.getElementById("calc-are")
-
 // Makes the default calculator be showed on the screen when the user press the default button
 function set_default() {
 
+}
+// Basic Calculator functions
+function insert(char) {
+    let display = document.getElementById("result")
+    if (display.innerText.length >= 15) {
+        alert("[ERROR] More than 15 characters are not allowed")
+    } else {
+        display.innerText += char
+    }
+}
+function evaluate() {
+    let calc = document.getElementById("result")
+    let operations = calc.innerText
+    calc.innerText = eval(operations)
 }
 
 // For the Atom calculator:
